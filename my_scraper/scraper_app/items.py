d03f036990ccf568125b70338987933b49e386b5
Jc Tecklenburg <jt9816a@student.american.edu>
2014-06-08 10:09:59 -0400

    got part 1 of newcoder.io to work finally. i probably forget python

--- my_scraper/scraper_app/items.py
+++ my_scraper/scraper_app/items.py
@@ -1,4 +1,17 @@
 from scrapy.item import Item, Field
 
 class LivingSocialDeal(Item):
-	"""Livingsocial container (dictionary-like object) for scraped data"""
+	# Livingsocial container (dictionary-like object) for scraped data
+	title = Field()
+	location = Field()
+	# description = Field()
+	# link = Field()
+	category = Field()
+    # original_price = Field()
+    # price = Field()
+
+# deal = LivingSocialDeal(title="$20 off yoga classes", category="health")
+# print deal
+# print deal['title']
+# deal['location'] = "New York"
+# print deal['location']
---
+++ my_scraper/scraper_app/spiders/livingsocial_spider.py
@@ -0,0 +1,3 @@
+from scrapy.spider import BaseSpider
+
+from scraper_app.items import LivingSocialDeal

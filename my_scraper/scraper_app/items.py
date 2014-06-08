a284c78065a4112aa84cdc83bbd1c181cb60a4c6
Jc Tecklenburg <jt9816a@student.american.edu>
2014-06-08 10:58:16 -0400

    scrape part 2 tutorial complete. I think I undertand most of it. hopefully i'll come back on my next scrape and actually internalize it and then make my own

--- my_scraper/scraper_app/items.py
+++ my_scraper/scraper_app/items.py
@@ -1,16 +1,16 @@
 from scrapy.item import Item, Field
 
 class LivingSocialDeal(Item):
-	# Livingsocial container (dictionary-like object) for scraped data
+	"Livingsocial container (dictionary-like object) for scraped data"
 	title = Field()
 	location = Field()
-	# description = Field()
-	# link = Field()
+	description = Field()
+	link = Field()
 	category = Field()
-    # original_price = Field()
-    # price = Field()
+	price = Field()
+	original_price = Field()
 
-#
+# Test Statements
 # deal = LivingSocialDeal(title="$20 off yoga classes", category="health")
 # print deal
 # print deal['title']
